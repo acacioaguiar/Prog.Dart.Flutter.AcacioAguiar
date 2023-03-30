a4be1a54b7e187a40ed4b321637773b3850049c8
acacioaguiar <acacio1986@yahoo.com.br>
2023-03-30 19:26:23 -0300

    Print terceira Avaliação

---
+++ avaliacoes/avaliacao-03/Atividade3_Operacoes_Mat.dart
@@ -0,0 +1,47 @@
+// Atividade 3
+// Aluno Acacio Aguiar
+// Data: 30/03/2023
+//Resultado das operações matemáticas
+
+void main() {
+
+
+print('1) Qual o resultado da expressão 5 + 3 * 2?');
+
+int resultado0 = 0;
+resultado0 = 5+3*2;
+print ('O resultado é: $resultado0');
+
+
+print ('2) Qual o resultado da expressão 10 / 2 - 3?');
+
+double resultado1 = 0;
+resultado1 = 10/2-3;
+print ('O resultado é: $resultado1');
+
+
+print ('3) Qual o resultado da expressão 7 % 3?');
+
+double resultado2 = 0;
+resultado2 = 7%3;
+print ('O resultado é: $resultado2');
+
+//4) Qual o valor de x após a execução da expressão x += 5?
+
+
+//5) Qual o valor de y após a execução da expressão y *= 3?
+
+
+//6) Qual o resultado da expressão !(2 < 5) || (3 > 1)?
+
+
+//7) Qual o valor de z após a execução da expressão z ?? 10?
+
+
+//8) Qual o resultado da expressão 2 + 2 == 4 && 3 + 3 == 6?
+
+
+//9) Qual o resultado da expressão 5 < 3 || 4 > 2 && 6 != 6?
+
+//10 ) Qual o valor de a após a execução da expressão a ??= 10?
+}
